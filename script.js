754c73f1587bfc83e0d1dd2ff861f9c20b9ccae8
김예환 Ye-Hwan Kim (Sam) <yehwankim23@gmail.com>
2022-12-18 22:30:32 +0900

    Add accounts

--- script.js
+++ script.js
@@ -14,3 +14,27 @@ const MARKER = new kakao.maps.Marker({
 
 MARKER.setMap(MAP);
 MAP.addControl(new kakao.maps.ZoomControl(), kakao.maps.ControlPosition.RIGHT);
+
+const ACCOUNTS = {
+  groom: "성이름 ㅇㅇ은행 00000000000000",
+  groomFather: "성이름 ㅇㅇ은행 00000000000000",
+  groomMother: "성이름 ㅇㅇ은행 00000000000000",
+  bride: "성이름 ㅇㅇ은행 00000000000000",
+  brideFather: "성이름 ㅇㅇ은행 00000000000000",
+  brideMother: "성이름 ㅇㅇ은행 00000000000000",
+};
+
+[...document.getElementsByClassName("account")].forEach((ELEMENT) => {
+  ELEMENT.addEventListener("click", () => {
+    const ACCOUNT = ACCOUNTS[ELEMENT.id];
+
+    navigator.clipboard.writeText(ACCOUNT).then(
+      () => {
+        alert("계좌번호를 복사했습니다.\n" + ACCOUNT);
+      },
+      () => {
+        alert("계좌번호를 복사하지 못했습니다.\n" + ACCOUNT);
+      }
+    );
+  });
+});
